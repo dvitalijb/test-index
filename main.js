304a8142361bae00729d98cb4ba87d1204f047ea
vitalii <dumavb777@gmail.com>
2019-05-21 19:05:49 +0300

    witout submit

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const inputNameUser = document.querySelector('input[type="text"]');
 const telephoneUser = document.querySelector('input[type="tel"]');
 const selectRegions = document.getElementById('regions');
-
+const selectCities = document.getElementById('cities');
+const checkboxPrefer = document.querySelector('input[type="checkbox"]');
+const citiesOfRegion = {
+    'center': ['', 'Cherkasy', 'Dnipro', 'Kropyvnytskyi', 'Poltava', 'Vinnytsia', 'Zhytomyr', 'Not in the list'],
+    'North':['', 'Chernihiv', 'Sumy', 'Not in the list'],
+    'East': ['', 'Donetsk', 'Kharkiv', 'Luhansk', 'Not in the list'],
+    'South':['', 'Kherson', 'Mykolaiv', 'Odesa', 'Zaporizhzhia', 'Not in the list'],
+    'West': ['', 'Chernivtsi', 'Ivano-Frankivsk', 'Khmelnytskyi', 'Lutsk', 'Lviv', 'Rivne', 'Ternopil', 'Uzhhorod', 'Not in the list']
+}
 
 function checkInputName() {
 
     this.addEventListener('blur', function () {
         const valueThis = this.value.match(/\S+/g);
 
-        console.log(this)
         if (valueThis) {
             const lengthValue = valueThis.length;
             if (lengthValue < 2 || lengthValue > 3) {
@@ -48,21 +55,35 @@ function checkInputTelephone() {
 }
 
 function showSelectCities() {
-    const citiesOfRegion = {
-        'center': ['Cherkasy', 'Dnipro', 'Kropyvnytskyi', 'Poltava', 'Vinnytsia', 'Zhytomyr'],
-        'North':['Chernihiv', 'Sumy'],
-        'East': ['Donetsk', 'Kharkiv', 'Luhansk'],
-        'South':['Kherson', 'Mykolaiv', 'Odesa', 'Zaporizhzhia'],
-        'West': ['Chernivtsi', 'Ivano-Frankivsk', 'Khmelnytskyi', 'Lutsk', 'Lviv', 'Rivne', 'Ternopil', 'Uzhhorod']
+
+
+    while (selectCities.hasChildNodes()) {
+        selectCities.removeChild(selectCities.firstChild);
     }
     const valueSelectRegions = this.value;
     const citiesRegion = citiesOfRegion[valueSelectRegions];
     if(citiesRegion) {
-        console.log(citiesRegion)
+
+        citiesRegion.forEach(city => {
+
+            const tegSelect = document.createElement('option');
+
+            tegSelect.textContent = city;
+            tegSelect.setAttribute('value', city);
+            selectCities.append(tegSelect);
+
+        })
     }
 
+}
+
+function hideDomElements() {
+     telephoneUser.classList.toggle('hideElement');
+    selectRegions.classList.toggle('hideElement');
+   selectCities.classList.toggle('hideElement');
 }
 
 inputNameUser.addEventListener('focus', checkInputName);
 telephoneUser.addEventListener('focus', checkInputTelephone);
 selectRegions.addEventListener('click', showSelectCities);
+checkboxPrefer.addEventListener('click', hideDomElements);
